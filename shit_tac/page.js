e2c1e4e8777e83dd510b28493d5d54126315a747
Rodrigo Peleias <rpeleias@hotmail.com>
2015-12-10 22:49:26 -0200

    First version: winner - need some adjusts

--- shit_tac/page.js
+++ shit_tac/page.js
@@ -10,18 +10,22 @@ $(document).ready(function() {
 			this.board = [];
 			this.player = '',
 			this.computerPlayer = '';
-			$('.square').empty();			
+			$('.row-column').remove();			
 			for(var possibility = 0; possibility < 9; possibility++) {				
 				var appendedDiv = $('<div class="col-xs-4 row-column" id="' + possibility + '"></div>');
 				$('.square').append(appendedDiv);
 				this.board[possibility] = {element: appendedDiv, player: ''};				
-			}			
+			}
+			$('.row-column').on('click', function() {				
+				game.playerTurn(this);				
+			});		
 		}
 
 		this.playerTurn = function(clickedElement) {
 			if($(clickedElement).find("img").length <= 0) {
 				$(clickedElement).prepend($('<img>',{id:'poop',src: game.playersImage[game.player], class: 'img-responsive img-size'}));	
-				this.board[$(clickedElement).attr("id")].player = game.player;						
+				this.board[$(clickedElement).attr("id")].player = game.player;
+				this.checkWinner(game.player);				
 				this.computerTurn();		
 			}  else {
 				if(this.allSpacesAreFilled() === true){
@@ -47,6 +51,7 @@ $(document).ready(function() {
 					console.log(randomChoice.player);
 					if(randomChoice.player === '') {
 						$(randomChoice.element).prepend($('<img>',{id:'poop',src: this.playersImage[this.computerPlayer], class: 'img-responsive img-size'}));	
+						this.checkWinner(game.computerPlayer);
 						this.board[randomNumber].player = game.computerPlayer;	
 						notFilled = false;					
 					}
@@ -78,16 +83,31 @@ $(document).ready(function() {
 			$('#skullButton').one('click', function () {
 				game.player = "skull";
 				game.computerPlayer = "shit";
-			});
+			});			
+		}
+
+		this.checkWinner = function(player) {			
+			this.checkCombination(0,1,2, player);
+			this.checkCombination(3,4,5, player);
+			this.checkCombination(6,7,8, player);
+			this.checkCombination(0,3,6, player);
+			this.checkCombination(1,4,7, player);
+			this.checkCombination(2,5,8, player);
+			this.checkCombination(0,4,8, player);
+			this.checkCombination(2,4,6, player);
+		}
+
+		this.checkCombination = function(comb1, comb2, comb3, player) {
+			if((this.board[comb1].player === player) && (this.board[comb2].player === player) && (this.board[comb3].player === player)) {
+				alert("There is a winner!");
+				game.showPlayersOptions();
+				this.init();
+			}
 		}
 	};
 
 	game = new Game();	
 	game.showPlayersOptions();
 
-	game.init();
-  
-	$('.row-column').on('click', function() {		
-		game.playerTurn(this);				
-	});
+	game.init();	
 });
